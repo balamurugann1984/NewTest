de871af8d408e84110d2330094039d27c79e576f
Balamurugan_Gnanam <Balamurugan_Gnanam@USABOS139471L.ad.infosys.com>
2019-09-10 00:16:16 -0500

    commit changes

---
+++ E2ECITEST/src/TestRunner/TestRunnercheck.java
@@ -0,0 +1,18 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+		
+		features = "C:\\Users\\balamurugan_gnanam\\git\\NewTest\\E2ECITEST\\Features\\NewCI.feature"
+		
+		)
+
+
+public class TestRunnercheck {
+
+}
